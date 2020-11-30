8b347174fe836b0c6a88e75ed695d474b1f43a2e
Anon7250 <74374523+Anon7250@users.noreply.github.com>
2020-11-29 16:07:13 -0800

    Starter

---
+++ precache-manifest.14d91b71a77ec2d9d6571fb25ca2d26c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a040a4673fffd089e364a99d27a81b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f4c3b24424bf5a453e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d545f8f4d3c21e5a62c3",
+    "url": "/static/css/main.33074c3d.chunk.css"
+  },
+  {
+    "revision": "33f4c3b24424bf5a453e",
+    "url": "/static/js/2.fb49ce2e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fb49ce2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d545f8f4d3c21e5a62c3",
+    "url": "/static/js/main.b3b9f7d4.chunk.js"
+  },
+  {
+    "revision": "4e5341fed53818b6a68a",
+    "url": "/static/js/runtime-main.349d49a6.js"
+  }
+]);
